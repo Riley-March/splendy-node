92722748d1748582ad962605c36132f75b5582c0
Riley-March <drunkenbeard@outlook.com>
2020-04-21 21:18:50 +1000

    CircleCI conf changes

--- index.js
+++ index.js
@@ -17,26 +17,23 @@ const wss = new websocket.Server({ server })
 wss.on('connection', async (client) => {
     console.log('Client Connected');
     const event = await Event.find({}).sort({_id:-1}).limit(1);
-    client.send(JSON.stringify(event[0]));
+    client.send(JSON.stringify(event));
 });
 
 //Push updates to client every 30 seconds
 setInterval(async () => {
     wss.clients.forEach(async (client) => {
         const event = await Event.find({}).sort({_id:-1}).limit(1);
-        client.send(JSON.stringify(event[0]));
+        client.send(JSON.stringify(event));
     });
     console.log('Update Pushed');
 }, 30000);
 
 //Scrape the website every 10 minutes
-setInterval(async () => {
-    console.log('Scraping Website');
-    await scraper.get_groovin_tickets('https://www.moshtix.com.au/v2/event/groovin-the-moo-townsville-2020/117128');
-    await scraper.get_splendour_tickets('https://www.moshtix.com.au/v2/event/splendour-in-the-grass-2020/119191')
-}, 600000)
-
-console.log(process.env.MONGO_PASS)
+// setInterval(async () => {
+//     console.log('Scraping Website');
+//     await scraper.get_splendour_tickets('https://www.moshtix.com.au/v2/event/splendour-in-the-grass-2020/119191')
+// }, 600000)
 
 mongoose.connect(
     `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:27017/${process.env.MONGO_DB}?retryWrites=true&w=majority`
